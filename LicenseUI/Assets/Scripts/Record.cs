4fec8f2d09b46cf08ac73d6989e605efc76b6c67
mayk93 <mihai.mandrescu@gmail.com>
2015-03-22 20:50:37 +0200

    License infrastructure
    Started to implement UI for license project.

---
+++ LicenseUI/Assets/Scripts/Record.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Record : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+    {
+        AudioSource aud = GetComponent<AudioSource>();
+        aud.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+        aud.Play();
+	}
+	// Update is called once per frame
+	void Update () {}
+}
